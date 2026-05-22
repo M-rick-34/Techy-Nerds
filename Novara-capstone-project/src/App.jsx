7bbb82707f102fd246287bc3ba9ec92ba9f1985f
Daniel Aduba <danieladuba45@gmail.com>
2026-05-22 23:55:24 +0100

    Fixed final rendering and API export issues

--- Novara-capstone-project/src/App.jsx
+++ Novara-capstone-project/src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css';
-
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import PlanetSection from "./Components/PlanetSection";
+import VideoSection from "./Components/VideoSection";
+import ContactForm from "./Components/ContactForm";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Hero />
 
+      <PlanetSection />
+
+      <VideoSection />
+
+      <ContactForm />
+
+      <Footer />
     </>
   );
 }
