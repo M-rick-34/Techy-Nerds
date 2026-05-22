5ff3088083072ab7ed356a9ca568e330e2c6cbb2
Joshan04 <boluwatifejoshua4@gmail.com>
2026-05-22 06:52:11 -0400

    Create responsive footer component

--- Novara-capstone-project/src/Components/Footer.jsx
+++ Novara-capstone-project/src/Components/Footer.jsx
@@ -1,36 +1,45 @@
-function Footer() {
+import './Footer.css';
+
+const Footer = () => {
   return (
     <footer className="footer">
-      <div>
-        <h3>About The Team</h3>
+      <div className="footer-content">
+        <div className="footer-about">
+          <h3>About</h3>
+          <p>Josh, Mildred, Lekan, Ifeanyi, Olawale, Adebayo, Daniel, Divine</p>
+        </div>
 
-        <p>
-          We are a team of software engineering students
-          building a space exploration application.
-        </p>
-      </div>
+        <div className="footer-line"></div>
 
-      <div>
-        <a
-          href="https://tsacademyonline.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          TSAcademy
-        </a>
-      </div>
+        <div className="footer-bottom">
+          <div className="footer-copy">
+            <p>©2026 Designed by the Techy-Nerds</p>
 
-      <div>
-        <a
-          href="https://github.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Group Repository
-        </a>
+            <p>
+              Built by{' '}
+              <a
+                href="https://github.com/M-rick-34/Techy-Nerds.git"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Techy-Nerds
+              </a>
+              . All rights reserved
+            </p>
+          </div>
+
+          <a
+            className="footer-tsacademy"
+            href="https://tsacademyonline.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TSAcademy
+          </a>
+        </div>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
